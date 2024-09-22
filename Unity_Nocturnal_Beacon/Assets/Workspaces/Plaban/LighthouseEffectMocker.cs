bd9135aa9c931fcf225255b96e8040f0efd9645b
Plaban Biswas <plabanbiswas96@outlook.com>
2024-09-22 15:01:09 -0400

    - ADDED: 2D Spot light as Light Beam with alpha Fade to match panel alpha fade

--- Unity_Nocturnal_Beacon/Assets/Workspaces/Plaban/LighthouseEffectMocker.cs
+++ Unity_Nocturnal_Beacon/Assets/Workspaces/Plaban/LighthouseEffectMocker.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 using UnityEngine.UI;
 
 public class LighthouseEffectMocker : MonoBehaviour
@@ -17,10 +18,20 @@ public class LighthouseEffectMocker : MonoBehaviour
     [SerializeField] private float _currentAlpha;
 
 
+    [SerializeField] private Light2D _leftLight;
+    [SerializeField] private Light2D _rightLight;
+    [SerializeField] private Color _lightBeamColor;
+    [SerializeField] private AnimationCurve _lightBeamAlphaCurve;
+    [SerializeField] private float _currentLightBeamAlpha;
+
+
     private void Awake()
     {
         _panel = GetComponent<Image>();
-        _panel.color = GetColor(_initialAlpha);
+        _panel.color = GetPanelColor(_initialAlpha);
+
+        _lightBeamColor = _leftLight.color;
+        
     }
 
     private void Update()
@@ -35,12 +46,21 @@ public class LighthouseEffectMocker : MonoBehaviour
             var currentTimeSlice = Mathf.PingPong(Time.time, _effectDurationPerLoopInSecs) / _effectDurationPerLoopInSecs;
             _currentAlpha = _alphaCurve.Evaluate(currentTimeSlice);
 
-            _panel.color = GetColor(_currentAlpha);
+            _panel.color = GetPanelColor(_currentAlpha);
+
+            _currentLightBeamAlpha = _lightBeamAlphaCurve.Evaluate(currentTimeSlice);
+            _leftLight.color = GetLightBeamColor(_currentLightBeamAlpha);
+            _rightLight.color = GetLightBeamColor(_currentLightBeamAlpha);
         }
     }
 
-    private Color GetColor(float alpha)
+    private Color GetPanelColor(float alpha)
     {
         return new Color(_panel.color.r, _panel.color.g, _panel.color.b, alpha);
+    }
+    
+    private Color GetLightBeamColor(float alpha)
+    {
+        return new Color(_leftLight.color.r, _leftLight.color.g, _leftLight.color.b, alpha);
     }
 }
