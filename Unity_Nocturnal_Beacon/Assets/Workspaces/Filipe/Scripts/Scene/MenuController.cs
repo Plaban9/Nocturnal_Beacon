3c8b2b669f31d50aa0efc10ac662349477c8e82e
Filipe Mariz <51495797+fmmariz@users.noreply.github.com>
2024-11-07 23:26:18 -0500

    Commit 0711
    Main Menu with more work

--- Unity_Nocturnal_Beacon/Assets/Workspaces/Filipe/Scripts/Scene/MenuController.cs
+++ Unity_Nocturnal_Beacon/Assets/Workspaces/Filipe/Scripts/Scene/MenuController.cs
@@ -1,14 +1,25 @@
+using Minimalist.Audio;
+using Minimalist.Audio.Music;
+using Minimalist.Audio.Sound;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MenuController : MonoBehaviour
 {
     [SerializeField] public PlayerUnitData _DEBUG_DATA_FOR_PROTOTYPE1;
+
+    [SerializeField] public Slider masterVolume;
+    [SerializeField] public Slider musicVolume;
+    [SerializeField] public Slider sfxVolume;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        masterVolume.value = 1f;
+        musicVolume.value = 1f;
+        sfxVolume.value = 1f;
     }
 
     // Update is called once per frame
@@ -21,5 +32,21 @@ public class MenuController : MonoBehaviour
     {
         NoctBeaconRunData.Instance.SetPlayer(_DEBUG_DATA_FOR_PROTOTYPE1);
         SceneController.Instance.ToMap(); 
+    }
+
+    public void SetMasterVolume()
+    {
+        AudioManager.SetMasterVolume(masterVolume.value);
+
+    }
+
+    public void SetMusicVolume()
+    {
+        AudioManager.SetMusicVolume(musicVolume.value);
+    }
+
+    public void SetSFXVolume()
+    {
+        AudioManager.SetSFXVolume(sfxVolume.value);
     }
 }
