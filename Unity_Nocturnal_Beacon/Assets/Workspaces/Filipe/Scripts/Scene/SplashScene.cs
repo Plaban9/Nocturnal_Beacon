e5f6f2f5777784fa08e948ed71d31d65a54b3451
Filipe Mariz <51495797+fmmariz@users.noreply.github.com>
2024-12-01 17:57:03 -0500

    Added Splash

---
+++ Unity_Nocturnal_Beacon/Assets/Workspaces/Filipe/Scripts/Scene/SplashScene.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SplashScene : MonoBehaviour
+{
+    
+    public void ToMainMenu()
+    {
+        SceneController.Instance.ToMain();
+    }
+}
